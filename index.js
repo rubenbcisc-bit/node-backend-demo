fe4d61b5752773ce9c1e6cccc19160342c4c0b12
rubenbcisc-bit <rubenbcisc@gmail.com>
2026-01-20 10:33:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('¡Hola desde Gitpod con Node.js!');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
 
